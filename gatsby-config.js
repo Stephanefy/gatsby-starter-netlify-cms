d28ff5202ec77bfc57ab87d6e547fd96304b6cbe
Stephane <stephanefokyin@gmail.com>
2021-01-04 16:22:04 +0400

    feat: added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
 module.exports = {
   siteMetadata: {
     title: 'Auto-école Sainte-Clotilde',
+    steUrl: `https://www.autoecole-sainteclotilde.re`,
     description:
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     'gatsby-plugin-antd',
